7ae805e902b9e5d6f82b1a0eec52639bd4386bc8
Danny Wen <danny_wen@trendmicro.com>
2023-01-05 18:51:28 +0800

    Install tailwind css

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      'theme-1': '#F87070',
+      'theme-2': '#70F3F8',
+      'theme-3': '#D881F8',
+      'lilac-blue': '#D7E0FF',
+      'dark-blue-1': '#1E213F',
+      'dark-blue-2': '#161932',
+      'light-gray': '#EFF1FA',
+      white: '#FFFFFF',
+    },
+    fontFamily: {
+      sans: ['"Kumbh Sans"', 'sans-serif'],
+      serif: ['"Roboto Slab"', 'serif'],
+      mono: ['"Space Mono"', 'monospace'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
