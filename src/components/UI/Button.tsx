b32da903734a82b1778c60c312526b551642513e
Danny Wen <danny_wen@trendmicro.com>
2023-01-05 18:51:04 +0800

    Add new components

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <button className='bg-theme-1 hover:bg-[#fba2a2] transition ease-in text-white rounded-button w-[140px] h-[53px]'>
+      Apply
+    </button>
+  );
+}
---
+++ src/components/UI/Form.tsx
@@ -0,0 +1,37 @@
+import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/io';
+
+export default function Form({ title }: { title: string }) {
+  function handleOnChangeFont() {}
+
+  return (
+    <div className='bg-white w-fit p-1 grid gap-y-2.5 '>
+      <label
+        htmlFor={`${title}-Form`}
+        className=' text-dark-blue-1 text-xs opacity-40'
+      >
+        {title}
+      </label>
+      <div className='bg-light-gray rounded-[10px] w-[140px] h-[48px] p-4 flex items-center'>
+        <input
+          id={`${title}-Form`}
+          className='w-full bg-light-gray text-sm text-dark-blue-1 appearance-none outline-none'
+          type='number'
+          value='25'
+          onChange={handleOnChangeFont}
+        />
+        <div>
+          <IoIosArrowUp
+            tabIndex={0}
+            color='text-dark-blue-1'
+            className='hover:opacity-25 transition ease-in cursor-pointer'
+          />
+          <IoIosArrowDown
+            tabIndex={0}
+            color='text-dark-blue-1'
+            className='hover:opacity-25 transition ease-in cursor-pointer'
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
