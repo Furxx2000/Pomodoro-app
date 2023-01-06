4a27b44757b80cc63d98d68eab828da2d10e0001
Danny Wen <danny_wen@trendmicro.com>
2023-01-06 18:38:40 +0800

    Change file name to apply button

--- src/components/UI/Button.tsx
+++
@@ -1,7 +0,0 @@
-export default function Button() {
-  return (
-    <button className='bg-theme-1 hover:bg-[#fba2a2] transition ease-in text-white rounded-button w-[140px] h-[53px]'>
-      Apply
-    </button>
-  );
-}
