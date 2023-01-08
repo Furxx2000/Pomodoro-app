d4f67e7a1357ce8cd294104671fab09aaa1a277a
furxx2000 <furxx2000@gmail.com>
2023-01-08 23:51:23 +0800

    Create new components

---
+++ src/components/Layout/Pomodoro.tsx
@@ -0,0 +1,28 @@
+import Logo from '../UI/Logo';
+import ModeSwitcher from '../UI/ModeSwitcher';
+import Timer from '../UI/Timer';
+import Dialog from '../UI/Dialog/Dialog';
+import { useThemeState } from '../../hooks/useThemeState';
+import { IoSettingsSharp } from 'react-icons/io5';
+
+export default function Pomodoro() {
+  const { handleSetDialog } = useThemeState();
+
+  return (
+    <>
+      <main className='px-6 py-8 flex flex-col items-center gap-11'>
+        <Logo />
+        <ModeSwitcher />
+        <Timer />
+        <IoSettingsSharp
+          tabIndex={0}
+          color='#D7E0FF'
+          className='cursor-pointer outline-none'
+          size='28px'
+          onClick={handleSetDialog}
+        />
+      </main>
+      <Dialog />
+    </>
+  );
+}
