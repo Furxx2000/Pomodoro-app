ed04bb304f5ad37807084bdfa64634a0198f9d85
furxx2000 <furxx2000@gmail.com>
2023-01-08 23:53:24 +0800

    Use custom hook and create circular progress bar

--- src/components/UI/Timer.tsx
+++ src/components/UI/Timer.tsx
@@ -1,8 +1,26 @@
+import { useThemeState } from '../../hooks/useThemeState';
+
 export default function Timer() {
+  const { curTheme } = useThemeState();
+
   return (
-    <section className='grid place-items-center timer-size timer-gradient rounded-full relative'>
+    <section className='relative grid place-items-center timer-size timer-gradient rounded-full'>
       <div className='absolute grid place-content-center bg-dark-blue-2 rounded-full timer-center-size'>
-        <h1>17:59</h1>
+        <svg className='absolute w-[258px] h-[258px] place-self-center'>
+          <circle
+            cx='50%'
+            cy='50%'
+            r='119'
+            className='timer-progress-bar transition ease-in'
+          ></circle>
+          <circle
+            cx='50%'
+            cy='50%'
+            r='119'
+            className={`timer-progress-bar transition ease-in ${curTheme.color}`}
+          ></circle>
+        </svg>
+        <h1 className='text-[80px]'>17:59</h1>
         <h3>pause</h3>
       </div>
     </section>
