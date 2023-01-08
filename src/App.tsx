799b6672f563489cf7cd5db70a08491928c84fdd
furxx2000 <furxx2000@gmail.com>
2023-01-08 23:50:06 +0800

    Use Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import ApplyButton from './components/UI/Buttons/ApplyButton';
-import Form from './components/UI/Form';
-import Selector from './components/UI/Selector';
-import Logo from './components/UI/Logo';
-import ModeSwitcher from './components/UI/ModeSwitcher';
-import Timer from './components/UI/Timer';
-import Dialog from './components/UI/Dialog';
-import { IoSettingsSharp } from 'react-icons/io5';
+import { ThemeStateProvider } from './hooks/useThemeState';
+import Pomodoro from './components/Layout/Pomodoro';
 
 export default function App() {
   return (
-    <>
-      <main className='px-6 py-8 flex flex-col items-center gap-11'>
-        <Logo />
-        <ModeSwitcher />
-        <Timer />
-
-        <IoSettingsSharp
-          tabIndex={0}
-          color='#D7E0FF'
-          className='cursor-pointer'
-          size='28px'
-        />
-      </main>
-      <Dialog />
-    </>
+    <ThemeStateProvider>
+      <Pomodoro />
+    </ThemeStateProvider>
   );
 }
