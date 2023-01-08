a692f58ebdf5c0ebdce89423bcdc1318bb62f97c
furxx2000 <furxx2000@gmail.com>
2023-01-08 23:53:52 +0800

    Create new file for settings

---
+++ src/data/ThemeData.tsx
@@ -0,0 +1,49 @@
+export const Fonts = [
+  {
+    fontType: 'font-sans',
+    isSelected: true,
+  },
+  {
+    fontType: 'font-serif',
+    isSelected: false,
+  },
+  {
+    fontType: 'font-mono',
+    isSelected: false,
+  },
+];
+
+export const Colors = [
+  {
+    colorTheme: 'bg-theme-1',
+    isSelected: true,
+  },
+  {
+    colorTheme: 'bg-theme-2',
+    isSelected: false,
+  },
+  {
+    colorTheme: 'bg-theme-3',
+    isSelected: false,
+  },
+];
+
+export const Modes = [
+  {
+    modeType: 'pomodoro',
+    isSelected: true,
+  },
+  {
+    modeType: 'short break',
+    isSelected: false,
+  },
+  {
+    modeType: 'long break',
+    isSelected: false,
+  },
+];
+
+export const CurTheme = {
+  font: 'font-sans',
+  color: 'bg-theme-1',
+};
