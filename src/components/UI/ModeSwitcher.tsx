930b29aaaa1975dfb6f683798127abeed3d3ab88
furxx2000 <furxx2000@gmail.com>
2023-01-09 23:26:47 +0800

    Use curTheme custom state

--- src/components/UI/ModeSwitcher.tsx
+++ src/components/UI/ModeSwitcher.tsx
@@ -2,11 +2,11 @@ import { useThemeState } from '../../hooks/useThemeState';
 import ModeButton from './Buttons/ModeButton';
 
 export default function ModeSwitcher() {
-  const { modes } = useThemeState();
+  const { curTheme } = useThemeState();
 
   return (
     <section className='w-full h-[63px] max-w-[373px] rounded-[31.5px] bg-dark-blue-2 px-1.5 py-2 flex relative z-10'>
-      {modes.map((mode) => (
+      {curTheme.modes.map((mode) => (
         <ModeButton key={mode.modeType} mode={mode} />
       ))}
     </section>
