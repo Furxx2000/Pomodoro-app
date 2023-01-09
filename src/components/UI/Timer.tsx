c9343633edd23e098e5b7d4e076b26278bd68eb4
furxx2000 <furxx2000@gmail.com>
2023-01-09 23:27:04 +0800

    Add dynamic time

--- src/components/UI/Timer.tsx
+++ src/components/UI/Timer.tsx
@@ -2,6 +2,7 @@ import { useThemeState } from '../../hooks/useThemeState';
 
 export default function Timer() {
   const { curTheme } = useThemeState();
+  const curMode = curTheme.modes.filter((mode) => mode.isSelected)[0];
 
   return (
     <section className='relative grid place-items-center timer-size timer-gradient rounded-full'>
@@ -20,8 +21,8 @@ export default function Timer() {
             className={`timer-progress-bar transition ease-in ${curTheme.color}`}
           ></circle>
         </svg>
-        <h1 className={`text-[80px] ${curTheme.font}`}>17:59</h1>
-        <h3 className={`${curTheme.font}`}>pause</h3>
+        <h1 className={`text-[80px] ${curTheme.font}`}>{curMode.value}:00</h1>
+        <h3 className={`${curTheme.font}`}>start</h3>
       </div>
     </section>
   );
