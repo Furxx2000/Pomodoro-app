9f24f8b631afe93ed6e3d84ea42b4fc7a0d1d39d
Danny Wen <danny_wen@trendmicro.com>
2023-01-09 18:34:23 +0800

    Add functionality for Form and selector components

--- src/hooks/useThemeState.tsx
+++ src/hooks/useThemeState.tsx
@@ -66,6 +66,61 @@ export default function useStateSource() {
     setDialog(!dialog);
   }
 
+  function handleIncrease(modeName: string) {
+    const newArr = modes.map((mode) => {
+      if (+mode.value >= 99) return mode;
+      return {
+        ...mode,
+        value:
+          mode.modeType === modeName
+            ? (+mode.value + 1).toString()
+            : mode.value,
+      };
+    });
+    setMode(newArr);
+  }
+
+  function handleDecrease(modeName: string) {
+    const newArr = modes.map((mode) => {
+      if (+mode.value <= 1) return mode;
+      return {
+        ...mode,
+        value:
+          mode.modeType === modeName
+            ? (+mode.value - 1).toString()
+            : mode.value,
+      };
+    });
+    setMode(newArr);
+  }
+
+  function handleChangeMinsByInput(modeName: string, value: string) {
+    console.log(modeName, value);
+    const newArr = modes.map((mode) => {
+      return {
+        ...mode,
+        value: mode.modeType === modeName ? value : mode.value,
+      };
+    });
+    setMode(newArr);
+  }
+
+  function handleInvalidVal(modeName: string) {
+    const newArr = modes.map((mode) => {
+      return {
+        ...mode,
+        value: mode.modeType === modeName ? checkValue(mode.value) : mode.value,
+      };
+    });
+    setMode(newArr);
+  }
+
+  function checkValue(value: string) {
+    if (+value <= 0) return '1';
+    if (value[0] === '0' && +value > 0) return `${value[1]}`;
+    return value;
+  }
+
   return {
     dialog,
     fonts,
@@ -77,6 +132,10 @@ export default function useStateSource() {
     handleSetFont,
     handleSetMode,
     handleApplyTheme,
+    handleIncrease,
+    handleDecrease,
+    handleChangeMinsByInput,
+    handleInvalidVal,
   };
 }
 
