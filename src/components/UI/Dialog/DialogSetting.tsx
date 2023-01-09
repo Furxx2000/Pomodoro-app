2582a27c8ff76da29e4dcae91562f9449407e235
Danny Wen <danny_wen@trendmicro.com>
2023-01-09 18:33:41 +0800

    The color section should avoid border bottom style

--- src/components/UI/Dialog/DialogSetting.tsx
+++ src/components/UI/Dialog/DialogSetting.tsx
@@ -7,8 +7,8 @@ export default function DialogSetting({ title, children }: Props) {
   return (
     <section
       className={`flex flex-col items-center gap-4 ${
-        title === 'color' ? 'py-4' : 'py-5'
-      } border-b border-[#E3E1E1]`}
+        title === 'color' ? 'py-4' : 'py-5 border-b border-[#E3E1E1]'
+      }`}
     >
       <h4 className='text-dark-blue-2'>{title}</h4>
       {children}
