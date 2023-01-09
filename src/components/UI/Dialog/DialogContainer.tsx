bea6e4818a089be9f331f9df5d284f0653363218
Danny Wen <danny_wen@trendmicro.com>
2023-01-09 18:32:59 +0800

    Use pointer events none to replace invisible so animation works

--- src/components/UI/Dialog/DialogContainer.tsx
+++ src/components/UI/Dialog/DialogContainer.tsx
@@ -12,7 +12,7 @@ export default function DialogContainer({
       role='dialog'
       aria-label='Open settings'
       className={`dialog-width h-[549px] md:h-[494px] bg-white rounded-[15px] fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[calc(-50%-7px)] ${
-        dialog ? '' : 'opacity-0 invisible'
+        dialog ? '' : 'opacity-0 pointer-events-none'
       } transition ease-in z-30`}
     >
       {children}
