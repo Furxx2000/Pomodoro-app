0ac6dd311b2b3a8755985911a1dd494577c5c0b0
furxx2000 <furxx2000@gmail.com>
2023-01-15 22:56:16 +0800

    Add animation for progress bar css

--- src/components/UI/Timer/TimerProgressBar.tsx
+++ src/components/UI/Timer/TimerProgressBar.tsx
@@ -1,7 +1,11 @@
 import { useThemeState } from '../../../hooks/useThemeState';
 
 export default function TimerProgressBar() {
-  const { curTheme } = useThemeState();
+  const { curTheme, modes } = useThemeState();
+  const theme = +curTheme.activeState.filter((mode) => mode.isSelected)[0]
+    ?.value;
+  const mode = +modes.filter((mode) => mode.isSelected)[0]?.value;
+  const curValue = theme / (mode * 60);
 
   return (
     <svg className='absolute w-[258px] h-[258px] place-self-center'>
@@ -15,6 +19,12 @@ export default function TimerProgressBar() {
         cx='50%'
         cy='50%'
         r='119'
+        style={{
+          strokeDasharray: 745,
+          strokeDashoffset: `calc(745 - (745 * ${
+            +curValue.toFixed(3) * 100
+          } / 100)`,
+        }}
         className={`timer-progress-bar transition ease-in ${curTheme.color}`}
       ></circle>
     </svg>
