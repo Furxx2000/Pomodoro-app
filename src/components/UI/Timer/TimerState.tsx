c2ed8dd6a372b99bb4cf08d744c500a5c63496f6
Danny Wen <danny_wen@trendmicro.com>
2023-01-19 12:04:27 +0800

    Set hover style for text color

--- src/components/UI/Timer/TimerState.tsx
+++ src/components/UI/Timer/TimerState.tsx
@@ -3,9 +3,17 @@ import { useThemeState } from '../../../hooks/useThemeState';
 export default function TimerState() {
   const { curTheme } = useThemeState();
 
+  function textColor(color: string) {
+    if (color.includes('1')) return 'hover:text-theme-1';
+    if (color.includes('2')) return 'hover:text-theme-2';
+    if (color.includes('3')) return 'hover:text-theme-3';
+  }
+
   return (
     <h3
-      className={`timer-state ${curTheme.font} tracking-[15px] indent-[15px] transition ease-in z-10 md:text-base`}
+      className={`timer-state ${textColor(curTheme.color)} ${
+        curTheme.font
+      } tracking-[15px] indent-[15px] transition ease-in z-10 md:text-base`}
     >
       {curTheme.isTimerStart ? 'pause' : 'start'}
     </h3>
